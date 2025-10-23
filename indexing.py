47c5ef41fde5376b4d0c671ffd5c8815d95e0ef4
John Tucker <jtucker@spotify.com>
2025-10-23 07:17:08 -0400

    splitting

--- indexing.py
+++ indexing.py
@@ -1,11 +1,16 @@
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 def main():
     loader = DirectoryLoader("./data/", glob="**/*.txt", loader_cls=TextLoader)
     documents = loader.load()
-    print(f"Total characters: {len(documents[0].page_content)}")
-    print(documents[0].page_content[:500])
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    all_splits = text_splitter.split_documents(documents)
+    print(f"Split blog post into {len(all_splits)} sub-documents.")
 
 if __name__ == "__main__":
     main()
